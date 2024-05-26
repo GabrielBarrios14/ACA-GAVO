8a06de79c0f43e9eaf73e1b31ca64125a3e9a1c6
GabrielBarrios14 <gabriel.barrios@cun.edu.co>
2024-05-26 17:56:32 -0500

    se agrgan nuevas funciones y correccion de errores

--- scrip.js
+++ scrip.js
@@ -3,41 +3,76 @@ let currentItem = 8;
 
 loadMoreBtn.onclick = () => {
     let boxes = document.querySelectorAll('.box-container .box'); 
-    for(var i = currentItem; i < currentItem + 4 && i < boxes.length; i++){ 
+    for (var i = currentItem; i < currentItem + 4 && i < boxes.length; i++) { 
         boxes[i].style.display = 'inline-block';
     }
     currentItem += 4;
-    if(currentItem >= boxes.length){
-        loadMoreBtn.style.display ='none'; 
+    if (currentItem >= boxes.length) {
+        loadMoreBtn.style.display = 'none'; 
     }
 }
-//carrito
+
+// carrito
 const carrito = document.getElementById('carrito');
 const elementos1 = document.getElementById('lista-1');
 const lista = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document,getElementById('vaciar-carrito');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
 cargarEventListeners();
 
-function cargarEventListeners(){
+function cargarEventListeners() {
     elementos1.addEventListener('click', comprarElemento);
     carrito.addEventListener('click', eliminarElemento);
-    vaciarCarritoBtn.addEventListener('click',vaciarCarrito);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 }
 
 function comprarElemento(e) {
-    e.preventDefaul();
-    if(e.target.classList.contains('agregar-carrito')) {
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito')) {
         const elemento = e.target.parentElement.parentElement;
         leerDatosElemento(elemento);
     }
 }
 
-function leerDatosElemento(elemento){
-    cost infoElemento = {
+function leerDatosElemento(elemento) {
+    const infoElemento = {
         imagen: elemento.querySelector('img').src,
         titulo: elemento.querySelector('h3').textContent,
-        id: elemento.querySelector('a').getAttribute('data-id')
+        id: elemento.querySelector('a').getAttribute('data-id'),
+        precio: elemento.querySelector('.precio').textContent
     }
     insertarCarrito(infoElemento);
+}
+
+function insertarCarrito(elemento) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>
+        <img src="${elemento.imagen}" width=100 height=150px>
+    </td>
+    <td>
+        ${elemento.titulo}
+    </td>
+    <td>
+        ${elemento.precio}
+    </td>
+    <td>
+        <a href="#" class="borrar" data-id="${elemento.id}">x</a>
+    </td>
+    `;
+    lista.appendChild(row);
+}
+
+function eliminarElemento(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('borrar')) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function vaciarCarrito() {
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+    return false;
 }
